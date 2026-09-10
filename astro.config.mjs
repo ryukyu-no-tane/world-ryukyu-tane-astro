160b2746c71a10e1863eac4074c9b2ea082bb8db
Kaname Sadoyama <sady225@gmail.com>
2026-09-11 08:59:15 +0900

    feat: publish latest news article on world2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://world.ryukyu-tane.com',
+  site: 'https://world2.ryukyu-tane.com',
   i18n: {
     defaultLocale: 'ja',
     locales: ['ja', 'en'],
